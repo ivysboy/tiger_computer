d23686e7935041bd9ccc8a2227d97d6a8f8c3d79
xuwuyuan <xuwuyuan@corp.netease.com>
2020-12-01 01:37:35 +0800

    push

---
+++ view/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime, timedelta, timezone
+
+def hi_wuyuan(request):
+    return HttpResponse("hello world!")
+
+
+def hi_tiger(request):
+    context = {}
+    context['hello'] = 'hello world'
+    context['wuyuan'] = '你好'
+    now = datetime.now()
+    context['time'] = now  # 强制设置为UTC+8:00
+    context['timestamp'] = now.timestamp() * 1000
+    return render(request, 'tiger_homepage.html', context)
